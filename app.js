728225e3e10b87689943de839dc8339b1843acba
nberonda <nicolasbc0@gmail.com>
2023-03-01 19:31:18 -0300

    cambios en styles.css

--- app.js
+++ app.js
@@ -7,10 +7,10 @@ publicPath= path.resolve(__dirname, './public');
 
 app.use(express.static(publicPath));
 
-app.listen (3000, () => {
-    console.log ('funka 3000');
-});
-
 app.get ('/', function(req,res) {
     res.sendFile(path.resolve(__dirname, './views/home.html'));
+});
+
+app.listen (3000, () => {
+    console.log ('funka 3000');
 });
